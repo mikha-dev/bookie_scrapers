6881d14faaaa95468c6b11018137b477ac883cfa
Jonatan Almen <almen.jonatan@gmail.com>
2020-01-17 19:35:49 +0100

    add tests

--- scrapers/base/odds_cache.py
+++ scrapers/base/odds_cache.py
@@ -1,69 +1,21 @@
-import datetime
-import logging
 from pathlib import Path
-
-import pandas as pd
 
 
 class OddsCache:
-    def __init__(self, data_path: str, competition_name):
-        self.data_path = Path(data_path)
-        self.data_path.mkdir(exist_ok=True, parents=True)
-        self.competition_name = competition_name + ".csv"
+    def __init__(self, repository_callback, data_path: str = ""):
+        """
+        Args:
+            data_path (str): Optional, if specified will create directory specified
+            repository_callback: Method which will receive a list of odds difference
+        """
+        assert repository_callback, "Add callback that will retrieve odds differences"
+
+        if data_path:
+            self.base_path = Path(data_path)
+            self.base_path.mkdir(parents=True, exist_ok=True)
+
         self.cache = {}
+        self.repository_callback = repository_callback
 
-    @staticmethod
-    def _add_new_bookies(old_bookies, new_bookies):
-        # Add bookies that did not exist previously and save to cache
-        updated_df = pd.concat(
-            [new_bookies[~new_bookies.index.isin(old_bookies.index)], old_bookies]
-        )
-
-        return updated_df
-
-    def add_new_odds(self, url, odds):
-
-        new_updates = self._create_data_frame(odds)
-
-        # If game not yet seen add bookies to cache
-        if url not in self.cache:
-            self.cache[url] = new_updates
-            return
-
-        # Get previously bookies for selected url
-        old_updates = self.cache[url]
-
-        self.cache[url] = new_updates
-
-        # calculate the difference between cache and new odds
-        difference_df = new_updates[["1", "X", "2"]] - old_updates[["1", "X", "2"]]
-
-        # Get the index of rows that changed since last save
-        difference_index = (difference_df > 0).any(axis="columns")
-
-        # Select rows where changes occurred
-        difference_df = difference_df.loc[difference_index, :]
-
-        if difference_index.any():
-            logging.info(f"odds difference in:  {url}")
-
-        difference_df.columns = ["1Diff", "XDiff", "2Diff"]
-
-        merged_cached_df = new_updates[difference_index].merge(
-            difference_df, on="bookie"
-        )
-
-        merged_cached_df["url"] = url
-        merged_cached_df["fetched"] = datetime.datetime.utcnow()
-
-        if not merged_cached_df.empty:
-            return merged_cached_df.to_records()
-
-        return []
-
-    @staticmethod
-    def _create_data_frame(odds):
-        df = pd.DataFrame(odds, columns=["bookie", "1", "X", "2", "payout"])
-        df = df.set_index("bookie")
-        df = df.astype({"1": float, "X": float, "2": float})
-        return df
+    def add(self, odds: list):
+        self.repository_callback(odds)
