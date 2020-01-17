f3b13ebe20eeb013c0175c2b4c4f80f1b66cb19c
Jonatan Almen <almen.jonatan@gmail.com>
2020-01-17 23:01:55 +0100

    refactored added tests

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# import logging
-#
-# from scrapers.oddsportal.oddsportal import OddsPortal
-# logging.basicConfig(level=logging.INFO)
-#
-# config = {
-#     "url": "https://www.oddsportal.com/soccer/england/premier-league/",
-#     "data_path": "./data",
-#     "competition_name": "premier-league"
-# }
-#
-# premier_league_scraper = OddsPortal(**config)
-# premier_league_scraper.start()
-from scrapers.base.odds_cache import OddsCache
+import functools
+import logging
+from scrapers.oddsportal import OddsPortal
+from scrapers.repositories import csv_repository
 
-OddsCache(lambda: None, "./a")
+logging.basicConfig(level=logging.INFO)
+
+OddsPortal(
+    url="https://www.oddsportal.com/soccer/england/premier-league/",
+    repository_callback=functools.partial(csv_repository, data_path='data/pl.csv')
+).start()
